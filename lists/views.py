68cd55cb2e43ec11f258186602effe4136337354
Seb <sebastian.mohnke@moneysupermarket.com>
2019-09-13 12:59:25 +0100

    Added test for 2nd item

--- lists/views.py
+++ lists/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, "home.html")
+    return render(
+        request=request,
+        template_name="home.html",
+        context={
+            "new_item_text": request.POST.get("item_text", ""),
+        }
+    )
