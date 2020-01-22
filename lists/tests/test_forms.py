80dde0da7b2925519e848817dadcd13bf3d548f0
Seb <sebastian.mohnke@moneysupermarket.com>
2020-01-22 11:07:18 +0000

    Advanced forms done

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR, ExistingListItemForm
 from lists.models import Item, List
 
+
 class ItemFormTest(TestCase):
     def test_form_renders_item_text_input(self):
         form = ItemForm()
@@ -41,3 +42,9 @@ class ExistingListItemFormTest(TestCase):
         form = ExistingListItemForm(for_list=list_, data={'text': 'no twins!'})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])
+
+    def test_form_save(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={"text": "hi"})
+        new_item = form.save()
+        self.assertEqual(new_item, Item.objects.all()[0])
